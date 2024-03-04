7f63d7355e601c72a2157d1bfc0a5ac8fcec8ec1
sakkarin <sakkarin.s@comseven.com>
2024-03-04 21:56:43 +0700

    fix: ru localization encoding

--- TimeLocalization/Localizations/RuLocalization.cs
+++ TimeLocalization/Localizations/RuLocalization.cs
@@ -2,33 +2,33 @@ namespace TimeLocalization.Localizations
 {
     public class RuLocalization : ILocalization
     {
-        public string SecondAgo { get; set; } = "только что";
-        public string SecondsAgo { get; set; } = "{0} секунд назад";
-        public string MinuteAgo { get; set; } = "1 минуту назад";
-        public string MinutesAgo { get; set; } = "{0} минут назад";
-        public string HourAgo { get; set; } = "1 час назад";
-        public string HoursAgo { get; set; } = "{0} часов назад";
-        public string DayAgo { get; set; } = "1 день назад";
-        public string DaysAgo { get; set; } = "{0} дней назад";
-        public string WeekAgo { get; set; } = "1 неделю назад";
-        public string WeeksAgo { get; set; } = "{0} недель назад";
-        public string MonthAgo { get; set; } = "1 месяц назад";
-        public string MonthsAgo { get; set; } = "{0} месяцев назад";
-        public string YearAgo { get; set; } = "1 год назад";
-        public string YearsAgo { get; set; } = "{0} лет назад";
-        public string InSecond { get; set; } = "скоро";
-        public string InSeconds { get; set; } = "через {0} секунд";
-        public string InMinute { get; set; } = "через 1 минуту";
-        public string InMinutes { get; set; } = "через {0} минут";
-        public string InHour { get; set; } = "через 1 час";
-        public string InHours { get; set; } = "через {0} часов";
-        public string InDay { get; set; } = "через 1 день";
-        public string InDays { get; set; } = "через {0} дней";
-        public string InWeek { get; set; } = "через 1 неделю";
-        public string InWeeks { get; set; } = "через {0} недель";
-        public string InMonth { get; set; } = "через 1 месяц";
-        public string InMonths { get; set; } = "через {0} месяцев";
-        public string InYear { get; set; } = "через 1 год";
-        public string InYears { get; set; } = "через {0} лет";
+        public string SecondAgo { get; set; } = "С‚РѕР»СЊРєРѕ С‡С‚Рѕ";
+        public string SecondsAgo { get; set; } = "{0} СЃРµРєСѓРЅРґ РЅР°Р·Р°Рґ";
+        public string MinuteAgo { get; set; } = "1 РјРёРЅСѓС‚Сѓ РЅР°Р·Р°Рґ";
+        public string MinutesAgo { get; set; } = "{0} РјРёРЅСѓС‚ РЅР°Р·Р°Рґ";
+        public string HourAgo { get; set; } = "1 С‡Р°СЃ РЅР°Р·Р°Рґ";
+        public string HoursAgo { get; set; } = "{0} С‡Р°СЃРѕРІ РЅР°Р·Р°Рґ";
+        public string DayAgo { get; set; } = "1 РґРµРЅСЊ РЅР°Р·Р°Рґ";
+        public string DaysAgo { get; set; } = "{0} РґРЅРµР№ РЅР°Р·Р°Рґ";
+        public string WeekAgo { get; set; } = "1 РЅРµРґРµР»СЋ РЅР°Р·Р°Рґ";
+        public string WeeksAgo { get; set; } = "{0} РЅРµРґРµР»СЊ РЅР°Р·Р°Рґ";
+        public string MonthAgo { get; set; } = "1 РјРµСЃСЏС† РЅР°Р·Р°Рґ";
+        public string MonthsAgo { get; set; } = "{0} РјРµСЃСЏС†РµРІ РЅР°Р·Р°Рґ";
+        public string YearAgo { get; set; } = "1 РіРѕРґ РЅР°Р·Р°Рґ";
+        public string YearsAgo { get; set; } = "{0} Р»РµС‚ РЅР°Р·Р°Рґ";
+        public string InSecond { get; set; } = "СЃРєРѕСЂРѕ";
+        public string InSeconds { get; set; } = "С‡РµСЂРµР· {0} СЃРµРєСѓРЅРґ";
+        public string InMinute { get; set; } = "С‡РµСЂРµР· 1 РјРёРЅСѓС‚Сѓ";
+        public string InMinutes { get; set; } = "С‡РµСЂРµР· {0} РјРёРЅСѓС‚";
+        public string InHour { get; set; } = "С‡РµСЂРµР· 1 С‡Р°СЃ";
+        public string InHours { get; set; } = "С‡РµСЂРµР· {0} С‡Р°СЃРѕРІ";
+        public string InDay { get; set; } = "С‡РµСЂРµР· 1 РґРµРЅСЊ";
+        public string InDays { get; set; } = "С‡РµСЂРµР· {0} РґРЅРµР№";
+        public string InWeek { get; set; } = "С‡РµСЂРµР· 1 РЅРµРґРµР»СЋ";
+        public string InWeeks { get; set; } = "С‡РµСЂРµР· {0} РЅРµРґРµР»СЊ";
+        public string InMonth { get; set; } = "С‡РµСЂРµР· 1 РјРµСЃСЏС†";
+        public string InMonths { get; set; } = "С‡РµСЂРµР· {0} РјРµСЃСЏС†РµРІ";
+        public string InYear { get; set; } = "С‡РµСЂРµР· 1 РіРѕРґ";
+        public string InYears { get; set; } = "С‡РµСЂРµР· {0} Р»РµС‚";
     }
 }
